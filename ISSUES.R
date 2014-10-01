2490b0ee80edc081e918c1413a3d7a6c4885270c
bemama <benjamin.mack@kit.edu>
2014-10-01 20:09:00 +0200

    added ISSUES.R

---
+++ ISSUES.R
@@ -0,0 +1,4 @@
+# in master and develop (not in nmm !) but if it is LOO it should 
+# additionally estimate the performance metric pased on the aggregated 
+# hold out predictions, i.e. adding 
+update(...)
