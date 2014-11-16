68d2db71af130c4e905ed943f3c6a5be1d0a28fe
benmack <benjamin.mack@kit.edu>
2014-11-16 17:38:15 +0100

    functionality of dataForSummaryFunction extended (xUn, trainOcc-obj. allowed)

--- R/dataForSummaryFunction.R
+++ R/dataForSummaryFunction.R
@@ -3,7 +3,7 @@
 #' 
 #' @description Creates a data frame from an object of class \code{holdOutPredictions} that can be passsed to the \code{data} argument of \code{\link{puSummary}} function.
 #' 
-#' @param  x an object of class \code{\link{holdOutPredictions}}
+#' @param  x an object of class \code{\link{holdOutPredictions}} or \code{\link{trainOcc}}
 #' @param threshold threshold used to binarize the data
 #' @return a data frame that can be passed to the \code{data} argument of \code{\link{puSummary}} function.
 #' @examples
@@ -16,7 +16,15 @@
 #' d <- dataForSummaryFunction(hop)
 #' }
 #' @export
-dataForSummaryFunction <- function(x, threshold=0) {
+dataForSummaryFunction <- function(x, threshold=0, newUn=NULL) {
+  
+  if (class(x)=="trainOcc") {
+    x <- holdOutPredictions(x, aggregate=TRUE)
+  }
+  
+  if (!is.null(newUn)) {
+    x$un <- newUn
+  }
   
   obs <- puFactor( rep( c(1, 0), c(length(x$pos), length(x$un)) ), positive=1 )
   pos <- c(x$pos, x$un)
