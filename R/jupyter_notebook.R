b637fe9c286a82a83e270740c33cfbce9c518f29
benmack <knecmab@gmail.com>
2016-06-21 10:38:17 +0200

    del jupyter notebook

--- R/jupyter_notebook.R
+++
@@ -1,9 +0,0 @@
-#' Activate r-oneClass environment and calls jupyter notebook.
-#'
-#' @return NULL
-#' @export
-#'
-#' @examples
-jupyter_notebook <- function() {
-  system("activate r-oneClass & jupyter notebook")
-}
